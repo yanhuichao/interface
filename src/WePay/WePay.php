5e28aa1c086b2a85a31c5568b743a78a7c160443
yanhuichao <liuyingji.me@gmail.com>
2017-12-09 17:13:33 +0800

    add PayChannel
    add Pay Channels

---
+++ src/WePay/WePay.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ag
+ * Date: 2017/12/9
+ * Time: 下午4:44
+ */
+namespace Pay\WePay;
+class WePay {
+    public static function getPayChannelName() {
+        return 'WePay';
+    }
+}
