1c1e838576b051e513ccde9e9053cc4bbe1381be
yanhuichao <liuyingji.me@gmail.com>
2017-12-09 23:42:04 +0800

    add tag
    git tag

---
+++ src/Pay/AliPay/AliPay.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ag
+ * Date: 2017/12/9
+ * Time: 下午4:53
+ */
+namespace Pay\AliPay;
+class AliPay {
+    public static function getPayChannelName() {
+        return 'AliPay';
+    }
+}
